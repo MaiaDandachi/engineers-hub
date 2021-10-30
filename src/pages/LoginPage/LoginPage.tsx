c36c8ae719e7a2fcfc86c902e8dbcc97dc13b69b
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2021-10-30 10:54:12 +0300

    Add base custom styles tailwindcss

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export const LoginPage: React.FC = () => (
-  <div className='bg-gray-100 min-h-screen pt-10 md:pt-20 pb-6 px-2 md:px-0'>
+  <div className='login-container'>
     <header className='max-w-lg mx-auto'>
       <h1 className='text-4xl text-center font-bold text-purple-700'>Engineers Hub</h1>
     </header>
-    <div className='bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl'>
+    <div className='login-card'>
       <section>
         <h2 className='font-bold text-2xl'>Welcome to Engineers Hub</h2>
-        <h3 className='text-gray-600 pt-2'>login to your account.</h3>
+        <h3 className='text-gray-600 pt-2'>Sign In to your account.</h3>
       </section>
 
       <div className='mt-8'>
@@ -17,40 +17,29 @@ export const LoginPage: React.FC = () => (
             <label htmlFor='email' className='block text-gray-700 text-sm font-bold mb-2 ml-3'>
               Email
             </label>
-            <input
-              type='email'
-              id='email'
-              className=' ml-0 bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-700 transition duration-500 px-3'
-            />
+            <input type='email' id='email' className='login-card__input' />
           </div>
           <div className='mb-6 pt-3 rounded bg-gray-200'>
             <label htmlFor='password' className='block text-gray-700 text-sm font-bold mb-2 ml-3'>
               Password
             </label>
-            <input
-              type='password'
-              id='password'
-              className=' ml-0 bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-700 transition duration-500 px-3'
-            />
+            <input type='password' id='password' className='login-card__input' />
           </div>
           <div className='text-gray-600 text-sm pb-3 hover:text-purple-800'>
             <a href='/'>Forgot your password?</a>
           </div>
-          <button
-            type='submit'
-            className='bg-purple-700 hover:bg-purple-800 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200'
-          >
-            Login
+          <button type='submit' className='login-card__submit'>
+            Sign In
           </button>
         </form>
       </div>
     </div>
     <div className='max-w-lg mx-auto text-center mb-5'>
       <p>
-        Don`t have an account?
+        Don&apos;t have an account?
         <a className='hover:text-purple-800' href='/'>
           {' '}
-          Register
+          Sign Up
         </a>
       </p>
     </div>
