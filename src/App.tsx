80172749479d0f9c0d99e368d5a09b5538fd74a5
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2021-11-01 21:55:30 +0300

    Add Signup page with onSubmit to json file

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { HomePage } from './pages/HomePage/HomePage';
 import { LoginPage } from './pages/LoginPage/LoginPage';
+import { RegisterPage } from './pages/RegisterPage/RegisterPage';
 
 const App: React.FC = () => (
   <Router>
     <Route path='/' component={HomePage} exact />
     <Route path='/login' component={LoginPage} />
+    <Route path='/register' component={RegisterPage} />
   </Router>
 );
 
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
-export const LoginPage: React.FC = () => {
+export const RegisterPage: React.FC = () => {
   const formik = useFormik({
     initialValues: {
+      userName: '',
       email: '',
       password: '',
+      confirmPassword: '',
     },
     validationSchema: Yup.object({
+      userName: Yup.string().required('Required'),
       email: Yup.string().email('Invalid email address').required('Required'),
       password: Yup.string().min(8, 'Must be 8 characters or more').required('Required'),
+      confirmPassword: Yup.string().oneOf([Yup.ref('password')], 'Passwords must match'),
     }),
-    onSubmit: (values) => {
+    onSubmit: (values, { resetForm }) => {
       fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify(values),
@@ -21,6 +25,7 @@ export const LoginPage: React.FC = () => {
           'Content-Type': 'application/json',
         },
       });
+      resetForm();
     },
   });
 
@@ -32,11 +37,28 @@ export const LoginPage: React.FC = () => {
       <div className='auth-card'>
         <section>
           <h2 className='font-bold text-2xl'>Welcome to Engineers Hub</h2>
-          <h3 className='text-gray-600 pt-2'>Sign In to your account.</h3>
+          <h3 className='text-gray-600 pt-2'>Sign Up to your account.</h3>
         </section>
 
         <div className='mt-8'>
           <form className='flex flex-col' onSubmit={formik.handleSubmit}>
+            <div className='mb-3 pt-3 rounded bg-gray-200'>
+              <label htmlFor='userName' className='block text-gray-700 text-sm font-bold mb-2 ml-3'>
+                UserName
+              </label>
+              <input
+                type='text'
+                id='userName'
+                name='userName'
+                className='auth-card__input'
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.userName}
+              />
+            </div>
+            {formik.touched.userName && formik.errors.userName ? (
+              <div className='mb-3 text-red-500'>{formik.errors.userName}</div>
+            ) : null}
             <div className='mb-3 pt-3 rounded bg-gray-200'>
               <label htmlFor='email' className='block text-gray-700 text-sm font-bold mb-2 ml-3'>
                 Email
@@ -71,21 +93,35 @@ export const LoginPage: React.FC = () => {
             {formik.touched.password && formik.errors.password ? (
               <div className='mb-3 text-red-500'>{formik.errors.password}</div>
             ) : null}
-            <div className='text-gray-600 text-sm pb-3 hover:text-purple-800'>
-              <a href='/'>Forgot your password?</a>
+            <div className='mb-3 pt-3 rounded bg-gray-200'>
+              <label htmlFor='password' className='block text-gray-700 text-sm font-bold mb-2 ml-3'>
+                Confirm Password
+              </label>
+              <input
+                type='password'
+                id='confirmPassword'
+                name='confirmPassword'
+                className='auth-card__input'
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.confirmPassword}
+              />
             </div>
+            {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+              <div className='mb-3 text-red-500'>{formik.errors.confirmPassword}</div>
+            ) : null}
             <button type='submit' className='auth-card__submit'>
-              Sign In
+              Sign Up
             </button>
           </form>
         </div>
       </div>
       <div className='max-w-lg mx-auto text-center mb-5'>
         <p>
-          Don&apos;t have an account?
-          <a className='hover:text-purple-800' href='/register'>
+          Already a member?
+          <a className='hover:text-purple-800' href='/login'>
             {' '}
-            Sign Up
+            Sign In
           </a>
         </p>
       </div>
