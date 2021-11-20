3d351c582263855cd53dcc42b4a42c91b1f7815e
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2021-11-20 14:55:16 +0300

    initialize userInfo state from local storage

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './redux-features/reducers';
 
-export const store = configureStore({ reducer });
+// JSON.parse requires a string be returned and not null from getItem
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo') || '{}')
+  : null;
+
+const preloadedState = {
+  users: { userInfo: userInfoFromStorage, error: null },
+};
+
+export const store = configureStore({ reducer, preloadedState });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
