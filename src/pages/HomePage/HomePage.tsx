8503fc239102ecb9d69bf8271e430a35522936a4
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2021-11-21 16:13:16 +0300

    add re routing for unregistered user

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useAppSelector } from '../../redux-features/hooks';
+import { Header } from '../../components/Header';
 
-export const HomePage: React.FC = () => <div>Home</div>;
+export const HomePage: React.FC = () => {
+  const { userInfo } = useAppSelector((state) => state.users);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push('/register');
+    }
+  }, [history, userInfo]);
+
+  return (
+    <>
+      <Header loggedInUserName={userInfo?.userName} />
+      <div>home</div>
+    </>
+  );
+};
