31103747cc90c5c873d34d87c48fbc9640c7b4f0
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2022-02-02 17:45:26 +0300

    Implement Notifictaions UI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,10 @@ import { Socket } from 'socket.io-client';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 
 import { MenuIcon } from '@heroicons/react/outline';
+import { BellIcon } from '@heroicons/react/solid';
 import { useAppDispatch } from '../../redux-features/hooks';
 import { logout } from '../../redux-features/users';
+import { NotificationsList } from '../Notifications/NotificationsList';
 
 interface IHeaderProps {
   loggedInUserName: string | undefined;
@@ -13,6 +15,7 @@ interface IHeaderProps {
 
 export const Header: React.FC<IHeaderProps> = ({ loggedInUserName, socket }) => {
   const [expanded, setExpanded] = useState(false);
+  const [isNotificationsListOpen, setIsNotificationListOpen] = useState(false);
   const dispatch = useAppDispatch();
 
   const handleLogout = () => {
@@ -27,8 +30,9 @@ export const Header: React.FC<IHeaderProps> = ({ loggedInUserName, socket }) => 
   }, [socket]);
 
   return (
-    <nav
-      className='
+    <>
+      <nav
+        className='
           flex
           items-center
           justify-between
@@ -39,25 +43,32 @@ export const Header: React.FC<IHeaderProps> = ({ loggedInUserName, socket }) => 
           bg-white
           shadow-md
         '
-    >
-      <div className='flex-grow'>
-        <span className=' bg-purple-700 px-4 py-1.5 text-lg text-white font-medium leading-5'>EngineersHub</span>
-      </div>
-      <ul className={`flex justify-end  text-gray-700 flex-grow md:flex ${expanded ? 'block' : 'hidden'} `}>
-        <li>
-          <span id='logged-in-userName' className='text-purple-600 font-medium text-lg'>
-            {loggedInUserName}
-          </span>
-        </li>
-        <li>
-          <button id='logout-button' type='button' onClick={handleLogout}>
-            <span className='ml-4 hover:text-purple-700 text-lg'>Logout</span>
-          </button>
-        </li>
-      </ul>
-      <button type='button' onClick={() => setExpanded(!expanded)} className='md:hidden ml-4 w-6 h-6 text-purple-700'>
-        <MenuIcon />
-      </button>
-    </nav>
+      >
+        <div className='flex-grow'>
+          <span className=' bg-purple-700 px-4 py-1.5 text-lg text-white font-medium leading-5'>EngineersHub</span>
+        </div>
+        <ul className={`flex justify-end  text-gray-700 flex-grow md:flex ${expanded ? 'block' : 'hidden'} `}>
+          <li>
+            <button type='button' onClick={() => setIsNotificationListOpen(!isNotificationsListOpen)}>
+              <BellIcon className='w-6 h-5 mt-1 hover:text-purple-600' />
+            </button>
+          </li>
+          <li>
+            <span id='logged-in-userName' className='text-purple-600 font-medium text-lg ml-4'>
+              {loggedInUserName}
+            </span>
+          </li>
+          <li>
+            <button id='logout-button' type='button' onClick={handleLogout}>
+              <span className='ml-4 hover:text-purple-700 text-lg'>Logout</span>
+            </button>
+          </li>
+        </ul>
+        <button type='button' onClick={() => setExpanded(!expanded)} className='md:hidden ml-4 w-6 h-6 text-purple-700'>
+          <MenuIcon />
+        </button>
+      </nav>
+      {isNotificationsListOpen && <NotificationsList />}
+    </>
   );
 };
---
+++ src/components/Notifications/NotificationsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const NotificationsList: React.FC = () => (
+  <div className='absolute z-10 bg-gray-200 rounded-md top-10 p-3 right-36 shadow-lg'>
+    <h1 className='text-xl font-bold '>Notifications</h1>
+    <div>NOTIFICATION 1</div>
+    <div>NOTIFICATION 2</div>
+    <div>NOTIFICATION 3</div>
+    <div>NOTIFICATION 4</div>
+  </div>
+);
